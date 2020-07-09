c40f97921392378d5b819959d3a0a283e8567c4d
tienbtm <tienbuithimy@admicro.vn>
2020-07-09 18:03:22 +0700

    initial commit

---
+++ src/main/java/com/example/sparkdemo/SparkdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sparkdemo/SparkdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sparkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
